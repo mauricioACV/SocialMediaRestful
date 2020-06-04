1b71b193774530c7b40aa43eeaeeb428af4ce043
mauricioACV <mauricio.campusano@gmail.com>
2020-06-04 18:02:11 -0400

    3 commit - se actualiza nuget a version 3.1 para hacer match con la version de .net core 3.1 - se agregan DTOs para no usar entidades directamente - se mapea por codigo sin usar autommaper (uso de autommaper proximo commit)

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using SocialMedia.Infrastructure.Repositories;
-using SocialMedia.Core.Interfaces;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SocialMedia.Api.Controllers
 {
@@ -11,28 +13,53 @@ namespace SocialMedia.Api.Controllers
     public class PostController : ControllerBase
     {
         private readonly IPostRepository _postRepository;
-        public PostController(IPostRepository postRepository)
+        private readonly IMapper _mapper;
+        public PostController(IPostRepository postRepository, IMapper mapper)
         {
             _postRepository = postRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetPost()
         {
             var post = await _postRepository.GetPosts();
-            return Ok(post);
+            var postsDto = post.Select(x => new PostDto
+            {
+                PostId = x.PostId,
+                Date = x.Date,
+                Description = x.Description,
+                Image = x.Image,
+                UserId = x.UserId
+            });
+            return Ok(postsDto);
         }
 
         [HttpGet ("{id}")]
         public async Task<IActionResult> GetPos(int id)
         {
             var post = await _postRepository.GetPost(id);
-            return Ok(post);
+            var postDto=new PostDto
+            {
+                PostId = post.PostId,
+                Date = post.Date,
+                Description = post.Description,
+                Image = post.Image,
+                UserId = post.UserId
+            };
+            return Ok(postDto);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(Post post)
+        public async Task<IActionResult> Post(PostDto postDto)
         {
+            var post = new Post
+            {
+                Date = postDto.Date,
+                Description = postDto.Description,
+                Image = postDto.Image,
+                UserId = postDto.UserId
+            };
             await _postRepository.InsertPost(post);
             return Ok(post);
         }
