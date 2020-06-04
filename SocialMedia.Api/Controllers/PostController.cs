5cb82fa0f211dedc96c1c9dbcc239cab51538c2a
mauricioACV <mauricio.campusano@gmail.com>
2020-06-04 13:21:45 -0400

    Primer commit, clean arquitecture - dependency injection - Api Fluente EF Core - Estructura de capas lista

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.Interfaces;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostRepository _postRepository;
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPost()
+        {
+            var post = await _postRepository.GetPosts();
+            return Ok(post);
+        }
+
+        [HttpGet ("{id}")]
+        public async Task<IActionResult> GetPos(int id)
+        {
+            var post = await _postRepository.GetPost(id);
+            return Ok(post);
+        }
+    }
+}
+
---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly SocialMediaContext _context;
+        public PostRepository(SocialMediaContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Post>> GetPosts()
+        {
+            var posts = await _context.Posts.ToListAsync();
+            return posts;
+        }
+        public async Task<Post> GetPost(int id)
+        {
+            var post = await _context.Posts.FirstOrDefaultAsync(x=> x.PostId==id);
+            return post;
+        }
+    }
+}
