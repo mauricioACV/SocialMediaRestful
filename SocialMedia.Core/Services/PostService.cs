babad0c39cb81c71d20de578a266bfb936992348
mauricioACV <mauricio.campusano@gmail.com>
2020-06-24 15:50:26 -0400

    se agrega primera implementacion de paginacion

--- SocialMedia.Core/Services/PostService.cs
+++ SocialMedia.Core/Services/PostService.cs
@@ -1,4 +1,5 @@
-﻿using SocialMedia.Core.Entities;
+﻿using SocialMedia.Core.CustomEntities;
+using SocialMedia.Core.Entities;
 using SocialMedia.Core.Exceptions;
 using SocialMedia.Core.Interfaces;
 using SocialMedia.Core.QueryFilters;
@@ -22,7 +23,7 @@ namespace SocialMedia.Core.Services
             return await _unitOfWork.PostRepository.GetById(id);
         }
 
-        public IEnumerable<Post> GetPosts(PostQueryFilter filters)
+        public PagedList<Post> GetPosts(PostQueryFilter filters)
         {
             var posts = _unitOfWork.PostRepository.GetAll();
 
@@ -41,7 +42,9 @@ namespace SocialMedia.Core.Services
                 posts = posts.Where(x => x.Description.ToLower().Contains(filters.Description.ToLower()));
             }
 
-            return posts;
+            var pagedPost = PagedList<Post>.Create(posts, filters.PageNumber, filters.PageSize);
+
+            return pagedPost;
         }
 
         public async Task InsertPost(Post post)
